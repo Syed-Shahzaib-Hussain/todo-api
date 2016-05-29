e9f37f720e6358abc5d08585c0b05d2ed3f24a02
Syed Shahzaib Hussain <syedshahzaibhussain123@gmail.com>
2016-05-29 20:03:50 +0500

    added todos and todos id

--- server.js
+++ server.js
@@ -3,7 +3,55 @@
  */
 var express = require('express');
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3001;
+var todos = [
+    {
+        id: 1,
+        description: 'Meeting with the mom',
+        completed: false
+    },
+    {
+        id:2,
+        description: 'Going for shopping',
+        completed: false
+    },
+    {
+        id:3,
+        description:'Going for date',
+        completed: true
+    }
+];
+
+app.get('/todos/:id', function (req, res) {
+    var todoID = parseFloat(req.params.id);
+    var matchedID;
+
+    todos.forEach(function (model) {
+        if (model.id === todoID) {
+            matchedID = model;
+        }
+    });
+    
+    // for (var i = 0; i<todos.length; i++ ) {
+    //     if (todos[i].id === todoID) {
+    //         matchedID = todos[i];
+    //     }
+    // }
+    //
+    if (matchedID) {
+        res.json(matchedID);
+    } else {
+        res.status(404).send();
+    }
+
+});
+
+
+
+
+app.get('/todos', function (req, res) {
+    res.json(todos)
+});
 
 app.get('/', function (req, res) {
     res.send("TODO API Root")
