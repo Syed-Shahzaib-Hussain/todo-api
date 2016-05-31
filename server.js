edeb442181c3911655332703c29705cba8cfafce
Syed Shahzaib Hussain <syedshahzaibhussain123@gmail.com>
2016-05-31 18:49:03 +0500

    Refactored API with Underscore-js

--- server.js
+++ server.js
@@ -7,18 +7,19 @@ var PORT = process.env.PORT || 3000;
 var todos = [];
 var todoNextId=1;
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 app.use(bodyParser.json());
 
 app.get('/todos/:id', function (req, res) {
     var todoID = parseFloat(req.params.id);
-    var matchedID;
+    var matchedID = _.findWhere(todos, {id: todoID});
 
-    todos.forEach(function (model) {
-        if (model.id === todoID) {
-            matchedID = model;
-        }
-    });
+    // todos.forEach(function (model) {
+    //     if (model.id === todoID) {
+    //         matchedID = model;
+    //     }
+    // });
     
     // for (var i = 0; i<todos.length; i++ ) {
     //     if (todos[i].id === todoID) {
@@ -46,8 +47,13 @@ app.get('/', function (req, res) {
 });
 
 app.post('/todos', function (req, res) {
-    var body = req.body;
+    var body = _.pick(req.body, 'description', 'completed');
+    if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+        return res.status(400).send();
+    }
+
     body.id = todoNextId++;
+    body.description = body.description.trim();
     todos.push(body);
     res.json(body);
     console.log(todos)
