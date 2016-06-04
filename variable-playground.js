1e67385ee6878af6d8c8ab1953e4ba30862ac942
Syed Shahzaib Hussain <syedshahzaibhussain123@gmail.com>
2016-06-04 11:07:18 +0500

    Added variable playground area

--- variable-playground.js
+++ variable-playground.js
@@ -1,3 +1,33 @@
 /**
  * Created by shahzaib on 6/2/16.
  */
+
+// var iObject = {
+//   name: "shahzaib",
+//     bankBalance: 50000
+// };
+//
+// function objChecker (obj) {
+//     // obj= {
+//     //     name:'habib',
+//     //     bankBalance:90000000
+//     // }
+//
+//     obj.name = "Habib Ullah";
+//     obj.balance = 10000000000;
+// }
+//
+// objChecker(iObject);
+// console.log(iObject);
+//
+//
+
+var iArray = ['shahzaib', 'abdullah', 'ghalib'];
+
+function arrayAdder(arrayName) {
+    // arrayName.push('Ali');
+    arrayName = ['shahzaib only']
+}
+
+arrayAdder(iArray);
+console.log(iArray);
